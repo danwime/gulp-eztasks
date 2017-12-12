417d3bd4b69d2c3a08c210f2b538484b145ad4e9
李雨田 <demon@danwi.me>
2017-12-12 11:01:05 +0800

    添加了npm任务

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
  * Created by demon on 16/5/11.
  */
 module.exports = {
-  babel: require('./tasks/babel'),
-  clean: require('./tasks/clean'),
-  sync: require('./tasks/sync'),
-  minijs: require('./tasks/minijs'),
-  minicss: require('./tasks/minicss'),
-  typscript: require('./tasks/typescript')
+    babel: require('./tasks/babel'),
+    clean: require('./tasks/clean'),
+    sync: require('./tasks/sync'),
+    minijs: require('./tasks/minijs'),
+    minicss: require('./tasks/minicss'),
+    typscript: require('./tasks/typescript'),
+    npm: require('./tasks/npm')
 };
---
+++ tasks/npm.js
@@ -0,0 +1,41 @@
+var util = require('gulp-util');
+var spawn = require('child_process').spawn;
+
+/**
+ * npm 脚本
+ * @param scriptName    任务脚本名
+ * @param workDir       工作目录
+ * @param title         任务标题
+ * @returns {Function}
+ */
+module.exports = function (scriptName, workDir, title) {
+    //默认参数
+    title = title || 'Npm[' + scriptName + ']';
+    workDir = workDir || '.';
+
+    return function (done) {
+        util.log('启动进程: ' + title + ' in ' + workDir);
+        if (scriptName) {
+            var ps = spawn('npm', ['run', scriptName], {cwd: workDir});
+            ps.stdout.on('data', function (data) {
+                console.log(data.toString())
+            });
+            ps.stderr.on('data', function (data) {
+                console.error(data.toString())
+            });
+            ps.on('close', function (code) {
+                if (code === 0) {
+                    util.log('进程 ' + title + ' 完成');
+                    done();
+                }
+                else {
+                    util.log('进程 ' + title + ' 异常退出, Code: ' + code);
+                    done(new Error('exit code is non-zero'))
+                }
+            });
+        } else {
+            util.log('请指定npm script名');
+            done(new Error('without script name'))
+        }
+    }
+};
